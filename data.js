9ee0a077a2e87df0552ab8a70c90c2ade9dbb67c
yusufvhora1 <112395450+yusufvhora1@users.noreply.github.com>
2022-11-07 11:28:33 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    mongoLink:'',//database link
+    
+    bot_token:'',//bot token
+
+    bot_name:'PayPalTokenGiveawayBot',//bot username without @
+    
+    bot_admin:1811781327,// admin telegram id
+    
+    channelsList : ['@bitfiction'],// channels list
+    
+    reffer_bonus:20000,// refferal bonus amount
+    
+    min_wd:20000,//min withdrawal amount
+    
+    daily_bonus:1,// daily bonus amount
+    
+    currency:'PPIN', //bot cuurency
+    
+    payment_channel:'@bitfiction'// payment channel username
+    
+    }
